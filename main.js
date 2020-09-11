76ea8e77644f3d80ec7d73db50cf0b9baff57fb5
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-09-11 11:45:37 -0400

    create product component

--- main.js
+++ main.js
@@ -1,30 +1,60 @@
-var app = new Vue({
-  el: "#app",
-  data: {
-    brand: "Vue Mastery",
-    product: "Socks",
-    selectedVariant: 0,
-    details: [
-      "80% cotton",
-      "20% polyester",
-      "Gender-neutral",
-    ],
-    variants: [
-      {
-        id: 1,
-        color: "green",
-        image: "./assets/vmSocks-green-onWhite.jpg",
-        quantity: 10,
-      },
-      {
-        id: 2,
-        color: "blue",
-        image: "./assets/vmSocks-blue-onWhite.jpg",
-        quantity: 0,
-      },
-    ],
-    cart: 0,
+Vue.component("product", {
+  template: `
+    <div class="product">
+      <div class="product-image">
+        <img v-bind:src="image" alt="Socks">
+      </div>
+
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+
+        <div class="color-box" :style="{ backgroundColor: variant.color}" v-for="(variant, index) in variants"
+          :key="variant.id" @mouseover=updateProduct(index)>
+        </div>
+
+        <button v-on:click="addToCart" :class="{disabledButton: !inStock}" :disabled="!inStock">Add to Cart</button>
+
+        <div class="cart">
+          <p>Cart({{ cart }})</p>
+        </div>
+      </div>
+    </div>
+  `,
+  
+  data() {
+    return {
+      brand: "Vue Mastery",
+      product: "Socks",
+      selectedVariant: 0,
+      details: [
+        "80% cotton",
+        "20% polyester",
+        "Gender-neutral",
+      ],
+      variants: [
+        {
+          id: 1,
+          color: "green",
+          image: "./assets/vmSocks-green-onWhite.jpg",
+          quantity: 10,
+        },
+        {
+          id: 2,
+          color: "blue",
+          image: "./assets/vmSocks-blue-onWhite.jpg",
+          quantity: 0,
+        },
+      ],
+      cart: 0,
+    };
   },
+
   methods: {
     addToCart() {
       this.cart += 1;
@@ -33,6 +63,7 @@ var app = new Vue({
       this.selectedVariant = index;
     },
   },
+
   // computed properties are special methods, which cache results and won't
   // rerun unless one of the dependent data has changed
   computed: {
@@ -46,4 +77,8 @@ var app = new Vue({
       return this.variants[this.selectedVariant].quantity;
     }
   }
+});
+
+var app = new Vue({
+  el: "#app",
 });
