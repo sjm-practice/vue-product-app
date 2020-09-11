8507fca384cbfc583d184d97ec1e2fed2df3804b
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-09-11 09:48:05 -0400

    add list rendering

--- main.js
+++ main.js
@@ -3,6 +3,21 @@ var app = new Vue({
   data: {
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpg",
-    prodWebsite: "https://www.amazon.com",
+    inStock: true,
+    details: [
+      "80% cotton",
+      "20% polyester",
+      "Gender-neutral",
+    ],
+    variants: [
+      {
+        id: 1,
+        color: "green",
+      },
+      {
+        id: 2,
+        color: "blue",
+      },
+    ],
   }
 });
