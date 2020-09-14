a93719ba1b7abae18f7bc0228013a8d75651b9ff
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-09-14 12:58:15 -0400

    add simple form validation

--- main.js
+++ main.js
@@ -115,6 +115,13 @@ Vue.component("product", {
 Vue.component("product-review", {
   template: `
     <form class="review-form" @submit.prevent="onSubmit">
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </p>
+
       <p>
         <label for="name">Name:</label>
         <input id="name" type="text" v-model="name"/>
@@ -147,23 +154,32 @@ Vue.component("product-review", {
       name: null,
       review: null,
       rating: null,
+      errors: [],
     };
   },
 
   methods: {
     onSubmit() {
-      let productReview = {
-        name: this.name,
-        review: this.review,
-        rating: this.rating,
-      };
+      this.errors = [];
 
-      this.$emit("review-submitted", productReview);
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+        };
 
-      this.name = null;
-      this.review = null;
-      this.rating = null;
-    }
+        this.$emit("review-submitted", productReview);
+
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+      } else {
+        if (!this.name) this.errors.push("Name required.");        
+        if (!this.review) this.errors.push("Review required.");        
+        if (!this.rating) this.errors.push("Rating required.");        
+      }
+  }
   }
 });
 
